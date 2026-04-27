4912547d5727e25c55160f69344b9e94e82b4ca5
uzma-04 <uzmaa0407@gmail.com>
2026-04-27 14:15:55 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE superstore (
+    row_id        INT PRIMARY KEY,
+    order_id      VARCHAR(20),
+    order_date    DATE,
+    ship_date     DATE,
+    ship_mode     VARCHAR(50),
+    customer_id   VARCHAR(20),
+    customer_name VARCHAR(100),
+    segment       VARCHAR(50),
+    country       VARCHAR(50),
+    city          VARCHAR(50),
+    state         VARCHAR(50),
+    postal_code   VARCHAR(20),
+    region        VARCHAR(20),
+    product_id    VARCHAR(20),
+    category      VARCHAR(50),
+    sub_category  VARCHAR(50),
+    product_name  VARCHAR(200),
+    sales         NUMERIC(10,2),
+    quantity      INT,
+    discount      NUMERIC(4,2),
+    profit        NUMERIC(10,2)
+);
